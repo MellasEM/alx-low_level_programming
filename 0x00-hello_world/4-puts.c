2514333925a4b90ce4b3c1f67b9dcf4e383ecffb
MellasEM <mohamed.elmellas@outlook.com>
2023-09-07 15:31:58 -0700

    Mandatory Tasks

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-
-/**                                                                                          
+/**                                                                                         
  * main - print Programming is like building a multilingual puzzle,  
  * Return: Always 0 (Success)
  */
