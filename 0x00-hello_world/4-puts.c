1d5acd3d6d0a23d39461ef38d47db95b7c8d9c62
MellasEM <mohamed.elmellas@outlook.com>
2023-09-07 15:41:52 -0700

    Mandatory Tasks

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-/**                                                                                         
- * main - print Programming is like building a multilingual puzzle,  
- * Return: Always 0 (Success)
- */
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle,\n");
