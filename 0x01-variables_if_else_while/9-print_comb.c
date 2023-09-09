466c8ff6ba1a7fa356d6c1200915d1cc3829ad5d
MellasEM <mohamed.elmellas@outlook.com>
2023-09-08 18:08:16 -0700

    Mandatory Tasks

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
-* main - a program that prints all possible combinations of single-digit numbers
+* main - all possible combinations of single-digit numbers
 * Return: 0
 */
 int main(void)
