2554d19f60e5c6eda02dc41aef7e4ae8d899d4da
MellasEM <mohamed.elmellas@outlook.com>
2023-09-08 17:22:36 -0700

    Mandatory Tasks

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - prints lowercase
+(*
+* Return: 0 (On Success)
+*/
+int main() {
+for (char c = 'a'; c <= 'z'; c++) {
+putchar(c);
+}
+putchar('\n');
+return 0;
+}
