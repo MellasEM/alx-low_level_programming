4223938dd06fd5857986a7f177fbf7b1a2fcbca6
MellasEM <mohamed.elmellas@outlook.com>
2023-09-13 19:36:32 -0700

    Mandotory Tasks

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_times_table - Prints the n times table, starting with 0.
  * @n: The times table to print.
@@ -7,7 +6,6 @@
 void print_times_table(int n)
 {
 int i, j, k;
-
 if (n >= 0 && n <= 15)
 {
 for (i = 0; i <= n; i++)
