ef21190b0e2fd897543d33f38e8e79ee90ca4d88
MellasEM <mohamed.elmellas@outlook.com>
2023-09-18 19:12:35 -0700

    Mandatory Tasks

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * _strlen - A function that returns the length of a string.
- *@n: String to check
- */
+* _strlen - A function that returns the length of a string.
+*@s: String to check
+* Return: lengh
+*/
 int _strlen(char *s)
 {
 int lengh = 0;
@@ -12,5 +13,5 @@ while (s[lengh] != '\0')
 {
 lengh++
 }
-	return (lengh);
+return (lengh);
 }
