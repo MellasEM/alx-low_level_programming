9b4d4f16ce0c5f7619c685664c2ac9f7a83d20d7
MellasEM <mohamed.elmellas@outlook.com>
2023-09-18 19:36:27 -0700

    Mandatory Tasks

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* print_rev - A function that prints a string, in reverse, 
+* print_rev - A function that prints a string, in reverse,
 *followed by a new line
 * @s: string
 * Return: 0
