2bbe48216a080fd0a0be16fe8bbcb17c6fdead17
MellasEM <mohamed.elmellas@outlook.com>
2023-09-19 18:22:30 -0700

    Mandatory Tasks

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - program that generates random valid passwords for the program 101-crackme.
+ * Return: Password
+ */
+int main(void)
+{
+char c;
+int x;
+
+srand(time(0));
+while (x <= 2645)
+{
+c = rand() % 128;
+x += c;
+putchar(c);
+}
+putchar(2772 - x);
+return (0);
+}
