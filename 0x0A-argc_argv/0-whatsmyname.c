29020e3c216b0dbe934fac780c6c825cb60a78d1
MellasEM <mohamed.elmellas@outlook.com>
2023-10-02 14:50:21 -0700

    Mandatory Tasks

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 		
-#include "main.h"
-			
+#include "main.h"	
 /**
  * main - prints the name of the program
  * @argc: number of argv array
- * @argv: array of arguments		
+ * @argv: array of arguments
  * Return: Always 0 (Success)
  */
 
