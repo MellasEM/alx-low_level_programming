b40e0d9f7472caea793cd10072e24ac5b436aa98
MellasEM <mohamed.elmellas@outlook.com>
2023-10-02 14:57:48 -0700

    Mandatory Tasks

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
-		
 #include "main.h"
 /**
  * main - prints the number of the program
  * @argc: number of argv array
- * @argv: array of arguments		
+ * @argv: array of arguments
  * Return: Always 0 (Success)
  */
 
-int main(int argc , char *argv[])
+int main(int argc, char *argv[])
 {
 (void) argv;
-printf("%d\n",argc - 1);
-return(0);
+printf("%d\n", argc - 1);
+return (0);
 }
