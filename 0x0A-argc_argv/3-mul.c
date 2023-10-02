f5f7be04408a1a739e459b8e1f9d60c61b538aa4
MellasEM <mohamed.elmellas@outlook.com>
2023-10-02 15:28:45 -0700

    Mandatory Tasks

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include "main.h"
 #include <stdlib.h>
-
 /**
  * main - multiplies two numbers from arguments
  * @argc: number of argv array
@@ -11,13 +10,15 @@
 
 int main(int argc, char *argv[])
 {
-int n1 = atoi(argv[1]);
-int n2 = atoi(argv[2]);
+int n1;
+int n2;
 if (argc != 3)
 {
 printf("Error\n");
 return (1);
 }
+n1 = atoi(argv[1]);
+n2 = atoi(argv[2]);
 printf("%d\n", n1 * n2);
 return (0);
 }
