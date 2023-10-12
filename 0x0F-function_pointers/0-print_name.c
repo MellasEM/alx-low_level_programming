ee6651253246c72e259fe6638e5e1bb95aa1d09b
MellasEM <mohamed.elmellas@outlook.com>
2023-10-11 19:38:31 -0700

    Mandatory Tasks

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ * print_name - function that prints a name.
+ * @name: the input
+ * @f: fucnitın pointer
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+if (name && f)
+f(name);
+}
