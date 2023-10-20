ddb083f3a2387f80c4a4915afec0cd619a7b3628
MellasEM <mohamed.elmellas@outlook.com>
2023-10-19 19:54:03 -0700

    Mandatory

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - unction that returns the number of elements
+ * in a linked list_t list.
+ * @h: pointer
+ * Return: number od elements
+ */
+
+ize_t list_len(const list_t *h)
+{
+size_t elemnts_number = 0;
+
+while (h)
+{
+h = h->next;
+elemnts_number++;
+}
+return (count);
+}
