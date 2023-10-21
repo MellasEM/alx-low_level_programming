0aa6c8deb0fea4aa00c5f36e01754156a075b407
MellasEM <mohamed.elmellas@outlook.com>
2023-10-20 17:46:53 -0700

    Mandatory

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * main_print - function that prints "You're beat! and yet, you must allow,
+ * \nI bore my house upon my back!\n before the main function is executed.
+ */
+
+void __attribute__((constructor)) main_print(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
