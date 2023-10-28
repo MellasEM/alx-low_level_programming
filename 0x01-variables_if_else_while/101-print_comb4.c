0c1c2d2c79fba8761ec114173ff4c7b6f09d523c
MellasEM <140663881+MellasEM@users.noreply.github.com>
2023-10-28 22:04:04 +0100

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,32 +5,37 @@
 */
 int main(void)
 {
-int i = '0', j = '0', k = '0';
-while (i <= '7')
-{
-while (j <= '8')
-{
-while (k <= '9')
-{
-if (i < j && j < k)
-{
-putchar(i);
-putchar(j);
-putchar(k);
-if (!(i == '7' && j == '8' && k == '9'))
-{
-putchar(',');
-putchar(' ');
-}
-}
-k++;
-}
-k = '0';
-j++;
-}
-j = '0';
-i++;
-}
-putchar('\n');
-return (0);
+  int i = '0', j = '0', k = '0';
+  
+  while (i <= '7')
+  {
+    while (j <= '8')
+    {
+      while (k <= '9')
+      {
+        if (i < j && j < k)
+        {
+          putchar(i);
+          putchar(j);
+          putchar(k);
+          if (!(i == '7' && j == '8' && k == '9'))
+          {
+            putchar(',');
+            putchar(' ');
+          }
+        }
+        
+        k++;
+      }
+      
+      k = '0';
+      j++;
+    }
+    
+    j = '0';
+    i++;
+  }
+  
+  putchar('\n');
+  return (0);
 }
