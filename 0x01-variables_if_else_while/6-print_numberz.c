fa22821a0a42698f4a8101f7511b7a2b0c5e2be2
MellasEM <140663881+MellasEM@users.noreply.github.com>
2023-10-28 08:22:09 +0100

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,14 +3,15 @@
 * main - Print Number
 * Return: 0 (On Success)
 */
+
 int main(void)
 {
-int i = '0';
-while (i <= '9')
-{
-putchar(i);
-i++;
-}
-putchar('\n');
-return (0);
+  int i = '0';
+  while (i <= '9')
+  {
+    putchar(i);
+    i++;
+  }
+  putchar('\n');
+  return (0);
 }
